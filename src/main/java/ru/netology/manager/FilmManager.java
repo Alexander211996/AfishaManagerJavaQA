bd54c60619411743f6bc995ba499f08ef0ec6e2e
Alexander211996 <rubolov.alexander@mail.ru>
2022-11-09 13:25:26 +0300

    Corrections

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -2,47 +2,33 @@ package ru.netology.manager;
 
 import ru.netology.domain.Film;
 
-import static java.lang.System.arraycopy;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class FilmManager {
-    private int filmsQuantity = 10;
-    private Film[] films = new Film[filmsQuantity];
-
-    public FilmManager(int filmsQuantity) {
-
-        this.filmsQuantity = filmsQuantity;
-    }
-
-    public FilmManager() {
-
-        this.filmsQuantity = 10;
-    }
+    private Film[] films = new Film[0];
 
     public void addNewFilm(Film film) {
         int length = films.length + 1;
         Film[] tmp = new Film[length];
-        for (int i = 0; i < films.length; i++) {
-            tmp[i] = films[i];
-        }
-        System.arraycopy (films, 0, tmp, 0, films.length);
+        System.arraycopy(films, 0, tmp, 0, films.length);
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = film;
         films = tmp;
     }
 
     public Film[] getLast10Films() {
-        Film[] result = new Film [filmsQuantity];
-        int counter = 0;
-        for (int i = 0; i < filmsQuantity; i++) {
+        Film[] result = new Film[films.length];
+        for (int i = 0; i < result.length; i++) {
             int index = films.length - i - 1;
-            result[counter] = films[index];
-            counter++;
+            result[i] = films[index];
         }
-
-        Film[] finalResult = new Film[counter];
-        arraycopy(result, 0, finalResult, 0, counter);
-
-        return finalResult;
+        return result;
     }
-
 }
+
--- src/test/java/ru/netology/manager/filmManager10FilmsTest.java
+++ src/test/java/ru/netology/manager/filmManager10FilmsTest.java
@@ -8,45 +8,45 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class filmManager10FilmsTest {
 
-    FilmManager manager = new FilmManager ();
+    FilmManager manager = new FilmManager();
 
-        Film first = new Film (1, "Avatar");
-        Film second = new Film (2, "Green Mile");
-        Film third = new Film(3, "Venom 2");
-        Film fourth = new Film (4, "Undisputed 4");
-        Film fifth = new Film (5, "Star Wars Episode 9");
-        Film sixth = new Film (6, "Warcraft");
-        Film seventh = new Film (7, "Supernatural");
-        Film eighth = new Film (8, "Predator");
-        Film ninth = new Film (9, "The Witcher");
-        Film tenth = new Film (10, "Thanos");
+    Film first = new Film(1, "Avatar");
+    Film second = new Film(2, "Green Mile");
+    Film third = new Film(3, "Venom 2");
+    Film fourth = new Film(4, "Undisputed 4");
+    Film fifth = new Film(5, "Star Wars Episode 9");
+    Film sixth = new Film(6, "Warcraft");
+    Film seventh = new Film(7, "Supernatural");
+    Film eighth = new Film(8, "Predator");
+    Film ninth = new Film(9, "The Witcher");
+    Film tenth = new Film(10, "Avengers");
+    Film eleventh = new Film(11, "Harry Potter");
 
-        @BeforeEach
-        void prepareFilmManager () {
-            manager.addNewFilm(first);
-            manager.addNewFilm(second);
-            manager.addNewFilm(third);
-            manager.addNewFilm(fourth);
-            manager.addNewFilm(fifth);
-            manager.addNewFilm(sixth);
-            manager.addNewFilm(seventh);
-            manager.addNewFilm(eighth);
-            manager.addNewFilm(ninth);
-            manager.addNewFilm(tenth);
+    @BeforeEach
+    void prepareFilmManager() {
+        manager.addNewFilm(first);
+        manager.addNewFilm(second);
+        manager.addNewFilm(third);
+        manager.addNewFilm(fourth);
+        manager.addNewFilm(fifth);
+        manager.addNewFilm(sixth);
+        manager.addNewFilm(seventh);
+        manager.addNewFilm(eighth);
+        manager.addNewFilm(ninth);
+        manager.addNewFilm(tenth);
 
-        }
+    }
 
-        @Test
-        public void shouldSave() {
-            manager.addNewFilm(first);
-            Film [] expected = new Film [] {
-                    first, tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second
-            };
+    @Test
+    public void shouldSaveNewFilm() {
+        manager.addNewFilm(eleventh);
+        Film[] expected = new Film[]{
+                eleventh, tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first
+        };
+        Film[] actual = manager.getLast10Films();
+        assertArrayEquals(expected, actual);
 
-            Film[] actual = manager.getLast10Films();
-            assertArrayEquals(expected, actual);
-
-        }
+    }
 
     @Test
     public void shouldGetAll() {
